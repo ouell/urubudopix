b9e684848c6a9cce21b84eb7f9c024614afbc932
Wellington M. Alves <wellmalves@gmail.com>
2023-07-06 23:03:43 -0300

    feat: create base entity with default properties

---
+++ src/UrubuDoPix.Domain/UrubuDoPix.Domain/Entities/Base/Entity.cs
@@ -0,0 +1,13 @@
+﻿namespace UrubuDoPix.Domain.Entities.Base;
+
+public abstract class Entity
+{
+    public Guid Id { get; }
+    public DateTime CreatedAt { get; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.Now;
+    }
+}
